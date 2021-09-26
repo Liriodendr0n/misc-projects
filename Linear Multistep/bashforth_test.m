c5b3fe6ab1bfb6beaea3788b6e39cc569475b9e2
Liriodendr0n <61613910+Liriodendr0n@users.noreply.github.com>
2021-09-25 23:30:03 -0500

    fix stability plots

--- Linear Multistep/bashforth_test.m
+++ Linear Multistep/bashforth_test.m
@@ -7,37 +7,55 @@ set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 
 %% model problem
+% polynomial for checking order
+% f = @(t, y) m*t.^(m-1); 
 
-m = 14;
+% r theta orbit IC = (rdot thetadot r theta)
+% f = @(t, y) [y(3).*y(2).^2 - 1./y(3).^2; -2./y(3) .*y(1).*y(2); y(1); y(2)];
 
-f = @(t, y) m*t.^(m-1);
+% x y orbit IC = (vx vy rx ry)
+f = @(t, y) [-y(3)./((y(3).^2 + y(4).^2).^(3/2)); -y(4)./((y(3).^2 + y(4).^2).^(3/2)); y(1); y(2)];
 
-y0 = zeros(1, 1);
-tspan = [0 3];
+y0 = [0; 1.2; 1; 0];
+tspan = [0 15];
 
-h = 0.03;
+h = 0.01;
 
 %% stencil and start
 
 stencil = [0 1 3 6 10 14 18 22 26 30 33 35 36];
-%stencil = [0 2 5 7];
+%stencil = [0 1 3 5 6];
 
 tstart = -(0:max(stencil))*h;
 
-ystart = exp(m.*tstart).*y0;
+opts = odeset('reltol', 1e-12);
+[t2, y2] = ode45(@(t, y) f(-t, y), -tstart, y0);
+start45 = ode45(@(t, y) f(-t, y), [0 (max(stencil)*h)], y0, opts);
+ystart = deval(start45, -tstart);
+
 
 %% test integrator
-
-[t, y, f] = bashforth(f, tspan, h, stencil, ystart);
-
+tic
+[t, y] = bashforth(f, tspan, h, stencil, ystart);
+toc
 %% plots
+
+% % for checking order with f = @(t, y) m*t.^(m-1);
+% figure
+% hold on
+% grid on
+% grid minor
+% set(gca, 'Yscale', 'log')
+% plot(t, abs(y - t.^(m)))
+% ylim([1e-17 1e17])
 
 figure
 hold on
+axis equal
 grid on
 grid minor
-set(gca, 'Yscale', 'log')
-plot(t, abs(y - t.^(m)))
-ylim([1e-17 1e17])
-
+% plot(y(3,:).*cos(y(4,:)), y(3,:).*sin(y(4,:)))
+plot(y(3,:), y(4,:))
+xlim([-2 2])
+ylim([-2 2])
 
--- Linear Multistep/classic_moulton.m
+++ Linear Multistep/classic_moulton.m
@@ -6,11 +6,11 @@ syms h x w z(w) phi xi yi t1 t2 t
 
 %% find coefficients
 
-x0 = 1-[36 35 33 30 26 22 18 14 10 6 3 1 0];
+stencil = [0 1 3 6 10 14 18 22 26 30 33 35 36];
 
-n = length(x0);
+n = length(stencil);
 
-lagrpol = @(j) prod((x - x0(1:end ~= j)')./(x0(j) - x0(1:end ~= j)'), 1);
+lagrpol = @(j) prod((x - stencil(1:end ~= j)')./(stencil(j) - stencil(1:end ~= j)'), 1);
 for i = 1:n
     if n == 1
         l = 2*x;
@@ -19,10 +19,15 @@ for i = 1:n
     end
 end
 
-b = int(l, x, 0, 1);
+bi = int(l, x, 0, 1);
+
+for i = 1:length(stencil)
+    b(stencil(i)+1) = bi(i);
+end
+n = length(b);
 
 s(phi) = exp(1i*phi);
-z(w) = (w^n - w.^(n-1))/sum(b.*w.^(n:-1:1));
+z(w) = (w^(n) - w.^(n-1))/sum(b.*w.^(1:n));
 
 f = matlabFunction(z(s));
 
@@ -36,8 +41,8 @@ grid minor
 axis equal
 fplot(xt, yt, [-pi pi])
 
-xlim([-1.5, 0.5])
-ylim([-1, 1])
+xlim([-5 5])
+ylim([-5 5])
 
 
 
